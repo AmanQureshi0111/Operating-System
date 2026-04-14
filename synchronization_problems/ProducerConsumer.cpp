cc6694c880726ffbae9191dc295908b26d2ded10
AmanQureshi0111 <amanq7362@gmail.com>
2026-04-14 14:44:19 +0530

    code update

--- synchronization_problems/ProducerConsumer.cpp
+++ synchronization_problems/ProducerConsumer.cpp
@@ -1,8 +1,8 @@
 #include <chrono>
+#include <condition_variable>
 #include <iostream>
 #include <mutex>
 #include <queue>
-#include <semaphore>
 #include <thread>
 
 using namespace std;
@@ -10,38 +10,39 @@ using namespace std;
 class ProducerConsumer {
   queue<int> buffer;
   const int buffer_size;
-  counting_semaphore<> empty_slots;
-  counting_semaphore<> filled_slots;
   mutex buffer_mutex;
+  condition_variable not_full;
+  condition_variable not_empty;
 
 public:
-  ProducerConsumer(int size)
-      : buffer_size(size), empty_slots(size), filled_slots(0) {}
+  ProducerConsumer(int size) : buffer_size(size) {}
 
   void producer(int items_to_produce) {
     for (int item = 1; item <= items_to_produce; item++) {
-      empty_slots.acquire();
       {
-        lock_guard<mutex> lock(buffer_mutex);
+        unique_lock<mutex> lock(buffer_mutex);
+        not_full.wait(lock, [this] {
+          return static_cast<int>(buffer.size()) < buffer_size;
+        });
         buffer.push(item);
         cout << "Produced: " << item << '\n';
       }
-      filled_slots.release();
+      not_empty.notify_one();
       this_thread::sleep_for(chrono::milliseconds(50));
     }
   }
 
   void consumer(int items_to_consume) {
     for (int i = 0; i < items_to_consume; i++) {
-      filled_slots.acquire();
       int item;
       {
-        lock_guard<mutex> lock(buffer_mutex);
+        unique_lock<mutex> lock(buffer_mutex);
+        not_empty.wait(lock, [this] { return !buffer.empty(); });
         item = buffer.front();
         buffer.pop();
         cout << "Consumed: " << item << '\n';
       }
-      empty_slots.release();
+      not_full.notify_one();
       this_thread::sleep_for(chrono::milliseconds(80));
     }
   }
--- synchronization_problems/main.cpp
+++ synchronization_problems/main.cpp
@@ -9,10 +9,16 @@ int main() {
   cout << "Enter buffer size and item count: ";
   cin >> buffer_size >> item_count;
 
+  if (buffer_size <= 0 || item_count < 0) {
+    cout << "Invalid input. Buffer size must be > 0 and item count must be >= "
+            "0.\n";
+    return 1;
+  }
+
   ProducerConsumer pc(buffer_size);
 
-  thread producer_thread(&ProducerConsumer::producer, &pc, item_count);
-  thread consumer_thread(&ProducerConsumer::consumer, &pc, item_count);
+  std::thread producer_thread(&ProducerConsumer::producer, &pc, item_count);
+  std::thread consumer_thread(&ProducerConsumer::consumer, &pc, item_count);
 
   producer_thread.join();
   consumer_thread.join();
